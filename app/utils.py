5f81183cb9c6188ecd27325df94a5018ad057ec2
Deepak Amro <deepak@amropartners.com>
2023-10-31 10:39:55 +0000

    'for testing'

--- app/utils.py
+++ app/utils.py
@@ -3,6 +3,8 @@ import streamlit as st
 
 @st.cache_data(show_spinner=False, ttl=3600)
 def pivot_df(df, index, columns, values):
+    duplicate_indices = df.index[df.index.duplicated(keep=False)]
+    print(f'Duplicate indices:  {duplicate_indices}')
     df_pivot = df.pivot(index=index, columns=columns, values=values).sort_index()
     df_pivot.columns.name = None
     return df_pivot
