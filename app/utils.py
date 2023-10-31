cb1b073e31392993cca3a0fa1e770117f1fb33b7
Deepak Amro <deepak@amropartners.com>
2023-10-31 10:42:23 +0000

    'added try except for testing'

--- app/utils.py
+++ app/utils.py
@@ -3,8 +3,13 @@ import streamlit as st
 
 @st.cache_data(show_spinner=False, ttl=3600)
 def pivot_df(df, index, columns, values):
-    duplicate_indices = df.index[df.index.duplicated(keep=False)]
-    print(f'Duplicate indices:  {duplicate_indices}')
-    df_pivot = df.pivot(index=index, columns=columns, values=values).sort_index()
-    df_pivot.columns.name = None
+    try:
+        df_pivot = df.pivot(index=index, columns=columns, values=values).sort_index()
+        df_pivot.columns.name = None
+    except Exception as e:
+        print(e)
+        duplicate_indices = df.index[df.index.duplicated(keep=False)]
+        print(duplicate_indices)
+        
+    
     return df_pivot
