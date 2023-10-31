f8102994237e36b6f9693513918ba9a0a5365920
Deepak Amro <deepak@amropartners.com>
2023-10-31 11:06:39 +0000

    'testing'

--- app/utils.py
+++ app/utils.py
@@ -3,13 +3,8 @@ import streamlit as st
 
 @st.cache_data(show_spinner=False, ttl=3600)
 def pivot_df(df, index, columns, values):
-    try:
-        df_pivot = df.pivot(index=index, columns=columns, values=values).sort_index()
-        df_pivot.columns.name = None
-    except Exception as e:
-        print(e)
-        duplicate_indices = df.index[df.index.duplicated(keep=False)]
-        print(duplicate_indices)
-        
-    
+    duplicate_indices = df.index[df.index.duplicated(keep=False)]
+    print(duplicate_indices)
+    df_pivot = df.pivot(index=index, columns=columns, values=values).sort_index()
+    df_pivot.columns.name = None
     return df_pivot
